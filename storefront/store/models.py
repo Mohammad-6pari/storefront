4603fe021b80fed73d351ba399552b031826f206
Mohammadreza Sheshpari <mohammad6pari@gmail.com>
2023-01-12 23:52:22 +0330

    models modified

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product' , on_delete = models.SET_NULL,null=True,related_name='+')
+    
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-    
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion,related_name='products') 
 
 
-class Costumer(models.Model):
-    
+class Customer(models.Model):
     MEMBERSHIP_BRONZE='B'
     MEMBERSHIP_SILVER='S'
     MEMBERSHIP_GOLD='G'
@@ -42,3 +51,24 @@ class Order(models.Model):
     
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT) 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.PositiveBigIntegerField()
+    unit_price = models.DecimalField(max_digits=6 , decimal_places=2)
+    
+    
+class Address(models.Model):
+    stree = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+                Customer,on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()

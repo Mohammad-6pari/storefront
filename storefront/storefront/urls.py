ca3bfcffcff672127298041ede5401af13ab8110
MohammadrezaSheshpari <msheshpari01@gmail.com>
2023-01-14 17:48:00 +0330

    format selected

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-
+from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]

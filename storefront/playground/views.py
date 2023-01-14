8840b9c089ffc2b92b4ef52b509710fe845ee20f
Mohammadreza Sheshpari <mohammad6pari@gmail.com>
2023-01-14 15:30:04 +0330

    admin view updated

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-# request handler
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
+
+from store.models import Order
 
 def say_hello(request):
-    return render(request,'hello.html',{'name':'Mosh'}) 
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set').order_by('-placed_at')[:5]
+    return render(request,'hello.html',{'name':'Mohammadreza','products':list(query_set)}) 
--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from . import models
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','unit_price','inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page= 10
     list_select_related = ['collection']
+    list_filter = ['collection','last_update']
     
     def collection_title(self,product):
         return product.collection.title
@@ -22,7 +25,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display=['first_name','last_name','membership']
     list_editable = ['membership']
     ordering = ['first_name','last_name']
-    
+    search_fields = ['first_name__istartswith','last_name__istartswith']
     
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -34,7 +37,8 @@ class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title','products_count']
     
     def products_count(self,collection):
-        return collection.products_count
+        url = reverse("admin:store_product_changelist") + '?' + urlencode({'collection__id':str(collection.id)})
+        return format_html('<a href={}> {} </a>',url,collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(

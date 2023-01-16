2ff6d40811ffebabd142c76e6892d11353454909
MohammadrezaSheshpari <msheshpari01@gmail.com>
2023-01-16 23:28:21 +0330

    settings of decimal in REST update

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'store',
     'tags',
     'likes'
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
